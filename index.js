e972645093f48ba577f2f1024d64dbc15f7b6750
Davogre <davogre@gmail.com>
2022-10-06 08:31:07 +0530

    First Challenge commit

---
+++ index.js
@@ -0,0 +1,10 @@
+// /**
+//  * CHALLENGE 1 
+//  * 
+//  * Declare variable "myObject" and assign value {}.
+//  * Print this variable to the console.
+//  * NOTE: variable "myObject" will NOT be reassigned in the future
+//  */
+
+const mynObject = {};
+console.log(mynObject);
