488f043b0140c92ff319a05ecf6a0f8020ed7499
Davogre <davogre@gmail.com>
2022-10-07 07:35:46 +0530

    second challenge

--- index.js
+++ index.js
@@ -10,16 +10,35 @@
 //  * Delete property "shared"
 //  */
 
-let myPost = {};
-myPost = {
-    postTitle: "Object is reference type",
-    postLikes: 0,
+// let myPost = {};
+// myPost = {
+//     postTitle: "Object is reference type",
+//     postLikes: 0,
 
-};
-myPost.shared = false;
-console.log(myPost);
+// };
+// myPost.shared = false;
+// console.log(myPost);
 
-myPost.postLikes = myPost.postLikes + 1;
-delete myPost.shared;
+// myPost.postLikes = myPost.postLikes + 1;
+// delete myPost.shared;
 
-console.log(myPost);
+// console.log(myPost);
+
+// /**
+//  * CHALLENGE 2
+//  * 
+//  * Create variable "myObject" and add property "a" - 10.
+//  * Create another variable "copyOfMyObject" and its value should be myObject.
+//  * Add new property "b" with value false to the "copyOfMyObject".
+//  * Print to the console "myObject" and "copyOfMyObject" and interpret results.
+//  */
+
+let mynObject = {};
+
+mynObject.a = 10;
+
+let copyOfmynObject = mynObject;
+copyOfmynObject.b = false;
+
+console.log(mynObject);
+console.log(copyOfmynObject);
