4ceb49e2f54f2d83d440ea8289ed99148b49b683
Davogre <davogre@gmail.com>
2022-10-10 07:11:44 +0530

    Challenge - 3

--- index.js
+++ index.js
@@ -30,15 +30,54 @@
 //  * And this function will return concatenated string.
 //  */
 
-function concatenateStrings(a, b) {
-    return a + b;
+// function concatenateStrings(a, b) {
+//     return a + b;
+// }
+
+// console.log(concatenateStrings("Hello ", "World")); // Hello World
+// // return bhi apne aap me saala variable hai jo value hold karta hai, 
+// // Aur use print karane ke liye
+// // Function call ko "Console.log" ke khopche me lena padta hai.
+// // console.log(functionCall());
+// // Ab
+// // print hoga "return" ke aage jo bhi hai.
+// // Agar "return" nahi banaya hhai to "Undefine " aayega fir result.
+
+
+// /** 
+//  * CHALLENGE 3
+//  * 
+//  * Create function "outerFunction" with 2 parameters.
+//  * Create function inside of the "outerFunction" and name it "innerFunction" with one parameter.
+//  * This "innerFunction" will return square of parameter (parameter * parameter).
+//  * 
+//  * In the "outerFunction" sum both parameters.
+//  * Call "innerFunction" with argument that is equal to sum of both parameters of the "outerFunction".
+//  * Log to the console result of the "innerFunction" call.
+//  */
+function outerFunction(a, b) {
+    const sum = a + b;
+
+    function innerFunction(c) {
+        return c * c;
+    }
+    const result = innerFunction(sum);
+    console.log(result); // or
+    console.log(innerFunction(sum));// Aese bhi likh sakte hai return ki value hoti hai poore function ke call me
 }
 
-console.log(concatenateStrings("Hello ", "World")); // Hello World
-// return bhi apne aap me saala variable hai jo value hold karta hai, 
-// Aur use print karane ke liye
-// Function call ko "Console.log" ke khopche me lena padta hai.
-// console.log(functionCall());
-// Ab
-// print hoga "return" ke aage jo bhi hai.
-// Agar "return" nahi banaya hhai to "Undefine " aayega fir result.
+outerFunction(2, 3);
+//console.log(innerFunction(sum));   kyu ki bahar nahi hai ye uoterfunction ke isliye Error to ayega
+
+// // // Simplified
+// // function outerFunction(a, b) {
+// //   function innerFunction(c) {
+// //     return c * c;
+// //   }
+
+// //   console.log(innerFunction(a + b));
+// // }
+
+// outerFunction(2, 3); // 25
+
+// outerFunction(3, 7); // 100
