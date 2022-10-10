d430be688b87557d2c395b7cdb0a8a5ca3058136
Davogre <davogre@gmail.com>
2022-10-10 11:29:30 +0530

    Function challenge setInterval setTimeout waala

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 /**
  * JS-BASICS/CORE/CHALLENGE 01: SOLUTION - Function Expressions
- * 
+ *
  * Use following built-in functions:
  * setTimeout
  * setInterval
  * clearInterval
- * 
+ *
  * You should print to the console ONLY 5 messages with interval 2 seconds:
  * "Here is message number 1"
  * "Here is message number 2"
@@ -16,11 +16,11 @@
 
 //Using setTimeout()
 
-setTimeout(() => { console.log("Here is message number 1") }, 2000);
-setTimeout(() => { console.log("Here is message number 2") }, 4000);
-setTimeout(() => { console.log("Here is message number 3") }, 6000);
-setTimeout(() => { console.log("Here is message number 4") }, 8000);
-setTimeout(() => { console.log("Here is message number 5") }, 10000);
+// setTimeout(() => { console.log("Here is message number 1") }, 2000);
+// setTimeout(() => { console.log("Here is message number 2") }, 4000);
+// setTimeout(() => { console.log("Here is message number 3") }, 6000);
+// setTimeout(() => { console.log("Here is message number 4") }, 8000);
+// setTimeout(() => { console.log("Here is message number 5") }, 10000);
 
 // Output:
 
@@ -29,3 +29,16 @@ setTimeout(() => { console.log("Here is message number 5") }, 10000);
 // Here is message number 3
 // Here is message number 4
 // Here is message number 5
+
+
+
+let i = 1;
+const myInterval = setInterval(() => {
+    console.log("Here is message number " + i);
+    i = i + 1;
+}, 2000);
+
+
+setTimeout(() => {
+    clearInterval(myInterval);
+}, 10000);
