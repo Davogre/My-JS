eb23e1f45047c3b9e271b9b97d9ac8f02003cd19
Davogre <davogre@gmail.com>
2022-10-10 08:26:17 +0530

    Example - 4

--- index.js
+++ index.js
@@ -1,83 +1,56 @@
 // /**
-//  * CHALLENGE 1
-//  *
-//  * Create function called "mult" and it will have 3 parameters.
-//  * Create new variable and assign to it result of multiplication of all 3 parameters.
-//  * Print to the console result.
-//  * Don't use "return".
+//  * EXAMPLE 1
+//  * 
+//  * Try to "declare" function expression
 //  */
+// // It is not possible to use function expression standalone
+// function() { } //Uncaught SyntaxError: Unexpected token (
 
-// function mult(a, b, c) {
-//     const d = a * b * c;
-//     console.log(d);
-// }
+// /**
+//  * EXAMPLE 2
+//  * 
+//  * Assign function expression to the variable
+//  */
+// const myFunction = function() { };
 
-// mult(2, 3, 4); //24
+// console.log(myFunction()); // undefined
 
-// mult(2, 3, 5); //30
+// console.log(myFunction); // ƒ () { }
 
-// mult(2, 3, "abc"); // NaN  You will get this value if you try to multiply anything with string
+// /**
+//  * EXAMPLE 3
+//  * 
+//  * Callback function
+//  */
+// setTimeout(function() {
+//   console.log("Delayed message");
+// }, 3000);
 
-// mult();  // NaN  Undefined * Undefined * Undefined  = NaN
+// Most Common use case of "  Anonymous Function Expression"
+// Function call using " Anonymous Function Expression" as ARGUMENT.
 
-// console.log(mult(2, 3, 5)); // 30 and Undefine Kyu ki return nahi hhai isliye. 30 is printed inside of the function and function returns "undefined"
+// Yaha pe dekh sakte hai ki "setTimeout" jo hai ek METHOD hai Global 
+// Object WINDOW ki.
+// WINDOW(global object hai) ki Method ko direct call kar sakte hai.
+
+// At least 1 argument istemaal kar sakto ho aap isme.
+// setTimeout() : iska game ye hai ki
+// pehla argument(jo function hai(jo Anonymous Function Expression hai))
+// usko call karta hai , jitne sec 1000ms, 2000ms jo bhi likho ge usme, 
+// utne time me call karta hai.
+// Call ander ka function automatically after 3 secs
 
 
 // /**
-//  * CHALLENGE 2
+//  * EXAMPLE 4
 //  * 
-//  * Create a function "concatenateStrings" and it will have 2 parameters.
-//  * And this function will return concatenated string.
+//  * Callback function (example 2)
 //  */
+let i = 1;
+setInterval(function () {
+    console.log("Message logged each 1 second " + i + " waala");
+    i = i + 1;
+}, 1000);
 
-// function concatenateStrings(a, b) {
-//     return a + b;
-// }
-
-// console.log(concatenateStrings("Hello ", "World")); // Hello World
-// // return bhi apne aap me saala variable hai jo value hold karta hai, 
-// // Aur use print karane ke liye
-// // Function call ko "Console.log" ke khopche me lena padta hai.
-// // console.log(functionCall());
-// // Ab
-// // print hoga "return" ke aage jo bhi hai.
-// // Agar "return" nahi banaya hhai to "Undefine " aayega fir result.
-
-
-// /** 
-//  * CHALLENGE 3
-//  * 
-//  * Create function "outerFunction" with 2 parameters.
-//  * Create function inside of the "outerFunction" and name it "innerFunction" with one parameter.
-//  * This "innerFunction" will return square of parameter (parameter * parameter).
-//  * 
-//  * In the "outerFunction" sum both parameters.
-//  * Call "innerFunction" with argument that is equal to sum of both parameters of the "outerFunction".
-//  * Log to the console result of the "innerFunction" call.
-//  */
-function outerFunction(a, b) {
-    const sum = a + b;
-
-    function innerFunction(c) {
-        return c * c;
-    }
-    const result = innerFunction(sum);
-    console.log(result); // or
-    console.log(innerFunction(sum));// Aese bhi likh sakte hai return ki value hoti hai poore function ke call me
-}
-
-outerFunction(2, 3);
-//console.log(innerFunction(sum));   kyu ki bahar nahi hai ye uoterfunction ke isliye Error to ayega
-
-// // // Simplified
-// // function outerFunction(a, b) {
-// //   function innerFunction(c) {
-// //     return c * c;
-// //   }
-
-// //   console.log(innerFunction(a + b));
-// // }
-
-// outerFunction(2, 3); // 25
-
-// outerFunction(3, 7); // 100
+//iski kahani aesi hai ki
+//har sec baad pehla argument chalayega, jo ki function call hai
