32e6d661ef32052476bf1ef90ddd032ab02c815e
Davogre <Davogre@gmail.com>
2022-11-07 01:04:38 +0530

    ScopeExamples

--- index.js
+++ index.js
@@ -32,13 +32,56 @@
 
 
 
-let i = 1;
-const myInterval = setInterval(() => {
-    console.log("Here is message number " + i);
-    i = i + 1;
-}, 2000);
+// let i = 1;
+// const myInterval = setInterval(() => {
+//     console.log("Here is message number " + i);
+//     i = i + 1;
+// }, 2000);
 
 
-setTimeout(() => {
-    clearInterval(myInterval);
-}, 10000);
+// setTimeout(() => {
+//     clearInterval(myInterval);
+// }, 10000);
+
+
+// 5. PRACTICE - Scopes - PART 1
+// console.log("Hello");
+// console.log("----------------");
+
+// var a = 10;
+// let b = true;
+// const c = 25;
+
+// function myFunction(a) {
+//     console.log(a);
+//     console.log(b);
+//     console.log(c);
+//     console.log("--------------");
+// }
+
+// function myFunction1(b) {
+//     console.log(a);
+//     console.log(b);
+//     console.log(c);
+//     console.log("----------------");
+// }
+
+// function myFunction2(c) {
+//     console.log(a);
+//     console.log(b);
+//     console.log(c);
+// }
+
+// myFunction();
+// myFunction1();
+// myFunction2();
+
+// Example - 2 Function Scope
+
+function myFunction(a,b){
+    const c = true;
+    console.log(c);
+    // console.log(d);
+}
+
+myFunction();
