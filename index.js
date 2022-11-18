049d50342c3f1a942ae6a7708aa17b4393a888bc
Davogre <Davogre@gmail.com>
2022-11-18 12:46:59 +0530

    LoopBranchCommit

--- index.js
+++ index.js
@@ -1,44 +1,13 @@
 /**
- * JS-BASICS/CORE/CHALLENGE 01: SOLUTION - Function Expressions
- *
- * Use following built-in functions:
- * setTimeout
- * setInterval
- * clearInterval
- *
- * You should print to the console ONLY 5 messages with interval 2 seconds:
- * "Here is message number 1"
- * "Here is message number 2"
- * "Here is message number 3"
- * "Here is message number 4"
- * "Here is message number 5"
+ * EXAMPLE 2
+ * 
+ * Regular for loop with arrays
+ * NEVER USE THIS KIND OF LOOPS WITH ARRAYS
  */
+const myArray = [0, true, "abc", null];
+myArray.i = "New property i"
 
-//Using setTimeout()
-
-// setTimeout(() => { console.log("Here is message number 1") }, 2000);
-// setTimeout(() => { console.log("Here is message number 2") }, 4000);
-// setTimeout(() => { console.log("Here is message number 3") }, 6000);
-// setTimeout(() => { console.log("Here is message number 4") }, 8000);
-// setTimeout(() => { console.log("Here is message number 5") }, 10000);
-
-// Output:
-
-// Here is message number 1
-// Here is message number 2
-// Here is message number 3
-// Here is message number 4
-// Here is message number 5
-
-
-
-let i = 1;
-const myInterval = setInterval(() => {
-    console.log("Here is message number " + i);
-    i = i + 1;
-}, 2000);
-
-
-setTimeout(() => {
-    clearInterval(myInterval);
-}, 10000);
+for (let i = 0; i < myArray.length; i++) {
+  console.log(myArray.i);
+}
+console.log(myArray);
